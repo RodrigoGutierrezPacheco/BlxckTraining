9033414bd9631ee9b87c105d30debff788b9b44e
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2023-03-05 18:09:26 -0600

    modificacion menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,11 +2,38 @@ import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/auth.context";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 
 function Navbar() {
   // Subscribe to the AuthContext to gain access to
   // the values from AuthContext.Provider's `value` prop
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
+
+	const customStyles = {
+		content: {
+		backgroundColor: "black",
+		color:"white",
+		fontSize:"100"
+		},
+	};
+
+	let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   return (
     // <nav>
@@ -40,10 +67,24 @@ function Navbar() {
     //     </>
     //   )}
     // </nav>
+		
 		<nav className="row">
 			<img className="logoNav l" src="images/pesaRusa.png" alt="" />
 			<h1 className="btNv">Blxck Trainig</h1>
-			<img className="logoNav o" src="images/menu.png" alt="" />
+			<img onClick={openModal} className="logoNav o" src="images/menu.png" alt="" />
+			<Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+				<div>
+					<h1>Menu</h1>
+					<h1>Mi Rutina</h1>
+					<h1>Contacto</h1>
+				</div>
+      </Modal>
 		</nav>
   );
 }
