7a25be7ec3c94003b09ec63db22143a4883ef1f9
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2023-03-06 18:05:29 -0600

    modificacion rutinas

--- src/pages/MiRutina/miRutina.jsx
+++ src/pages/MiRutina/miRutina.jsx
@@ -44,18 +44,6 @@ const MiRutina = () => {
 		})
 	}
 
-	function am(){
-		Swal.fire({
-			title: 'Press Pecho en Maquina',
-			imageUrl: 'images/pressPechoMaquina.jpeg',
-			imageWidth: 400,
-			imageHeight: 200,
-			imageAlt: 'Custom image',
-			confirmButtonText: "Entendido",
-			confirmButtonColor:"black",
-		})
-	}
-
  function user(){
  }
 
@@ -191,6 +179,102 @@ const MiRutina = () => {
 	})
  }
 
+ function extencionCuadriceps(){	
+	Swal.fire({
+	title: 'Extencion de Cuadriceps en Maquina',
+	imageUrl: 'images/extencionCuadriceps.webp',
+	imageWidth: 400,
+	imageHeight: 200,
+	imageAlt: 'Custom image',
+	confirmButtonText: "Entendido",
+	confirmButtonColor:"black",
+})
+}
+
+ function curlFemoralSentado(){	
+	Swal.fire({
+	title: 'Curl Femoral Sentado',
+	imageUrl: 'images/curlFemoralSentado.png',
+	imageWidth: 400,
+	imageHeight: 200,
+	imageAlt: 'Custom image',
+	confirmButtonText: "Entendido",
+	confirmButtonColor:"black",
+})
+}
+
+ function abduccion(){	
+	Swal.fire({
+	title: 'Abduccion en Maquina',
+	imageUrl: 'images/abduccionMAquina.webp',
+	imageWidth: 400,
+	imageHeight: 200,
+	imageAlt: 'Custom image',
+	confirmButtonText: "Entendido",
+	confirmButtonColor:"black",
+})
+}
+
+ function adduccion(){	
+	Swal.fire({
+	title: 'Aduccion en Maquina',
+	imageUrl: 'images/adduccionMaquina.jpg',
+	imageWidth: 400,
+	imageHeight: 200,
+	imageAlt: 'Custom image',
+	confirmButtonText: "Entendido",
+	confirmButtonColor:"black",
+})
+}
+
+function remoMaquina(){
+	Swal.fire({
+		title: 'Remo en Maquina',
+		imageUrl: 'images/remoMaquina.jpg',
+		imageWidth: 400,
+		imageHeight: 200,
+		imageAlt: 'Custom image',
+		confirmButtonText: "Entendido",
+		confirmButtonColor:"black",
+	})
+}
+
+function pulloverPoleaAlta(){
+	Swal.fire({
+		title: 'Pullover en Polea Alta',
+		imageUrl: 'images/pulloverPoleaAlta.jpg',
+		imageWidth: 400,
+		imageHeight: 200,
+		imageAlt: 'Custom image',
+		confirmButtonText: "Entendido",
+		confirmButtonColor:"black",
+	})
+}
+
+function hiperextencionesMaquina(){
+	Swal.fire({
+		title: 'Hiperextenciones en Maquina',
+		imageUrl: 'images/hiperextencionMaquina.jpg',
+		imageWidth: 400,
+		imageHeight: 200,
+		imageAlt: 'Custom image',
+		confirmButtonText: "Entendido",
+		confirmButtonColor:"black",
+	})
+}
+
+function aperturasMaquina(){
+	Swal.fire({
+		title: 'Aperturas en Maquina',
+		imageUrl: 'images/aperturasMaquina.jpg',
+		imageWidth: 400,
+		imageHeight: 200,
+		imageAlt: 'Custom image',
+		confirmButtonText: "Entendido",
+		confirmButtonColor:"black",
+	})
+}
+
 	const usuario = prompt("Ingresa tu Usuario")
 
   if(usuario==="Flor"||usuario==="flor"||usuario==="migue"||usuario==="Migue"){
@@ -203,53 +287,54 @@ const MiRutina = () => {
 			<Collapsible className='dia ' triggerWhenOpen="Semana 1"  triggerStyle={{fontSize:'5rem'}} trigger="Semana 1">
 				<div className='justify'>
 					<h1 className='red'>Dia 1 Hombro-Biceps-Triceps</h1>
-					<p>1.-<a onClick={pressMilitarMaquina}>▶️</a>Press militar en maquina <a className='red'>4 series 12/15 repeticiones</a></p>
-					<p>2.-<a onClick={elevacionesLateralesMancuerna}>▶️</a>Elevaciones laterales con mancuernas<a className='red'>4 series 12/15 repeticiones</a></p>
-					<p>3.-<a onClick={elevacionesFrontalesMartillo}>▶️</a>Elevaciones frontales con mancuernas<a className='red'>4 series 12/15 repeticiones</a></p>
-					<p>4.-<a onClick={curlBicepsMaquina}>▶️</a>Curl biceps en maquina<a className='red'>4 series 12/15 repeticiones</a></p>
-					<p>5.-<a onClick={curlBicepsMartilloMancuerna}>▶️</a>Curl biceps martillo con mancuerna<a className='red'>4 series 12/15 repeticiones</a></p>
-					<p>6.-<a onClick={fondosMaquina}>▶️</a>Fondos en maquina<a className='red'>4 series 12/15 repeticiones</a></p>
-					<p>7.-<a onClick={copa2Manos}>▶️</a>Copa a 2 manos<a className='red'>4 series 12/15 repeticiones</a></p>
+					<p>1.-<a onClick={pressMilitarMaquina}>▶️</a>Press militar en maquina <a className='red'> 4 series 12/15 repeticiones</a></p>
+					<p>2.-<a onClick={elevacionesLateralesMancuerna}>▶️</a>Elevaciones laterales con mancuernas<a className='red'> 4 series 12/15 repeticiones</a></p>
+					<p>3.-<a onClick={elevacionesFrontalesMartillo}>▶️</a>Elevaciones frontales con mancuernas<a className='red'> 4 series 12/15 repeticiones</a></p>
+					<p>4.-<a onClick={curlBicepsMaquina}>▶️</a>Curl biceps en maquina<a className='red'> 4 series 12/15 repeticiones</a></p>
+					<p>5.-<a onClick={curlBicepsMartilloMancuerna}>▶️</a>Curl biceps martillo con mancuerna<a className='red'> 4 series 12/15 repeticiones</a></p>
+					<p>6.-<a onClick={fondosMaquina}>▶️</a>Fondos en maquina<a className='red'> 4 series 12/15 repeticiones</a></p>
+					<p>7.-<a onClick={copa2Manos}>▶️</a>Copa a 2 manos<a className='red'> 4 series 12/15 repeticiones</a></p>
 				</div>
 				<hr />
 				<div className='justify'>
 					<h1>Dia 2 Pierna</h1>
-					<p>1.-Press pecho en maquina <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
-					<p>2.-Aperturas en maquina <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
-					<p>3.-Lagartijas 4 <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
-					<p>4.-Fondos en maquina <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
-					<p>5.-Copa a 2 manos <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
-					<p>6.-Jalones con cuerda en polea alta <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
+					<p>1.-<a onClick={extencionCuadriceps}>▶️</a>Extencion Cuadriceps en Maquina <a className='red'>4 series 15 repeticiones</a></p>
+					<p>2.-<a onClick={curlFemoralSentado}>▶️</a>Curl Femoral en Maquina<a className='red'>4 series 15 repeticiones</a></p>
+					<p>3.-<a onClick={abduccion}>▶️</a>Abdduccion<a className='red'>4 series 15 repeticiones</a></p>
+					<p>4.-<a onClick={adduccion}>▶️</a>Adduccion<a className='red'>4 series 15 repeticiones</a></p>
+					<p>5.-<a onClick={sentadillaMancuerna}>▶️</a>Sentadilla con Mancuerna<a className='red'>4 series 15 repeticiones</a></p>
+					<p>6.-<a onClick={pesoMuertoMancuerna}>▶️</a>Peso Muerto con Mancuerna<a className='red'>4 series 15 repeticiones</a></p>
 				</div>
 				<hr />
 				<div className='justify'>
-					<h1 className='red'>Dia 3 Hombro-Biceps</h1>
-					<p>1.-Press pecho en maquina <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
-					<p>2.-Aperturas en maquina <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
-					<p>3.-Lagartijas 4 <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
-					<p>4.-Fondos en maquina <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
-					<p>5.-Copa a 2 manos <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
-					<p>6.-Jalones con cuerda en polea alta <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
+					<h1 className='red'>Dia 3 Pecho-Espalda</h1>
+					<p>1.-<a onClick={pressPechoMaquina}>▶️</a>Press pecho en maquina<a className='red'>4 series 15 repeticiones</a></p>
+					<p>2.-<a onClick={aperturasMaquina}>▶️</a>Aperturas en maquina<a className='red'>4 series 15 repeticiones</a></p>
+					<p>3.-<a onClick={pulldownMaquina}>▶️</a>Pulldown en Maquina<a className='red'>4 series 15 repeticiones</a></p>
+					<p>4.-<a onClick={remoMaquina}>▶️</a>Remo en Maquina<a className='red'>4 series 15 repeticiones</a></p>
+					<p>5.-<a onClick={pulloverPoleaAlta}>▶️</a>Pullover Polea Alta<a className='red'>4 series 15 repeticiones</a></p>
+					<p>6.-<a onClick={hiperextencionesMaquina}>▶️</a>Hiperextenciones<a className='red'>4 series 15 repeticiones</a></p>
 				</div>
 				<hr />
 				<div className='justify'>
-					<h1>Dia 4 Espalda-Biceps</h1>
-					<p>1.-Press pecho en maquina <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
-					<p>2.-Aperturas en maquina <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
-					<p>3.-Lagartijas 4 <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
-					<p>4.-Fondos en maquina <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
-					<p>5.-Copa a 2 manos <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
-					<p>6.-Jalones con cuerda en polea alta <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
+				<h1 className='red'>Dia 4 Hombro-Biceps-Triceps</h1>
+					<p>1.-<a onClick={pressMilitarMaquina}>▶️</a>Press militar en maquina <a className='red'> 4 series 12/15 repeticiones</a></p>
+					<p>2.-<a onClick={elevacionesLateralesMancuerna}>▶️</a>Elevaciones laterales con mancuernas<a className='red'> 4 series 12/15 repeticiones</a></p>
+					<p>3.-<a onClick={elevacionesFrontalesMartillo}>▶️</a>Elevaciones frontales con mancuernas<a className='red'> 4 series 12/15 repeticiones</a></p>
+					<p>4.-<a onClick={curlBicepsMaquina}>▶️</a>Curl biceps en maquina<a className='red'> 4 series 12/15 repeticiones</a></p>
+					<p>5.-<a onClick={curlBicepsMartilloMancuerna}>▶️</a>Curl biceps martillo con mancuerna<a className='red'> 4 series 12/15 repeticiones</a></p>
+					<p>6.-<a onClick={fondosMaquina}>▶️</a>Fondos en maquina<a className='red'> 4 series 12/15 repeticiones</a></p>
+					<p>7.-<a onClick={copa2Manos}>▶️</a>Copa a 2 manos<a className='red'> 4 series 12/15 repeticiones</a></p>
 				</div>
 				<hr />
 				<div className='justify'>
 					<h1 className='red'>Dia 5 fullbody</h1>
-					<p>1.-Press pecho en maquina <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
-					<p>2.-Aperturas en maquina <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
-					<p>3.-Lagartijas 4 <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
-					<p>4.-Fondos en maquina <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
-					<p>5.-Copa a 2 manos <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
-					<p>6.-Jalones con cuerda en polea alta <a className='red'>4 series 15 repeticiones</a><a>▶️</a></p>
+					<p>1.-<a>▶️</a>Press pecho en maquina <a className='red'>4 series 15 repeticiones</a></p>
+					<p>2.-<a>▶️</a>Aperturas en maquina <a className='red'>4 series 15 repeticiones</a></p>
+					<p>3.-<a>▶️</a>Lagartijas 4 <a className='red'>4 series 15 repeticiones</a></p>
+					<p>4.-<a>▶️</a>Fondos en maquina <a className='red'>4 series 15 repeticiones</a></p>
+					<p>5.-<a>▶️</a>Copa a 2 manos <a className='red'>4 series 15 repeticiones</a></p>
+					<p>6.-<a>▶️</a>Jalones con cuerda en polea alta <a className='red'>4 series 15 repeticiones</a></p>
 				</div>
 		</Collapsible>
 		<Collapsible className='dia black1' trigger="Semana 2" triggerWhenOpen="Semana 2" triggerStyle={{fontSize:'5rem'}}>
