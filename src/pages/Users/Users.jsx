ee6bc93f382719bdc1cfa1dba5d99265d9f99172
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2023-06-20 14:37:10 -0600

    modificacion usuarios

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,13 +1,13 @@
 const users = [
-  { username: 'alejandra', password: '1' },
+  { username: 'alejandra', password: 'alejandra' },
   { username: 'pepe', password: 'DobleP' },
-  { username: 'daniel', password: '3' },
-  { username: 'flori', password: '4' },
-  { username: 'migue', password: '5' },
-  { username: 'itzi', password: '6' },
-  { username: 'eca', password: '7' },
-  { username: 'arroyo', password: '8' },
-	{ username: 'jesus', password: '9' },
+  { username: 'daniel', password: 'daniel' },
+  { username: 'flori', password: 'flori' },
+  { username: 'migue', password: 'migue' },
+  { username: 'itzi', password: 'itzi' },
+  { username: 'eca', password: 'eca' },
+  { username: 'arroyo', password: 'arroyo' },
+	{ username: 'jesus', password: 'jesus' },
 	{ username: 'dalet', password: 'dalet' },
 ];
 
