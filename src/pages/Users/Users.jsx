6393019f30df402177a33093839b80be41151960
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2023-06-20 10:56:31 -0600

    prueba usuarios

---
+++ src/pages/Users/Users.jsx
@@ -0,0 +1,13 @@
+const users = [
+  { username: 'alejandra', password: '1' },
+  { username: 'pepe', password: '2' },
+  { username: 'daniel', password: '3' },
+  { username: 'flori', password: '4' },
+  { username: 'migue', password: '5' },
+  { username: 'itzi', password: '6' },
+  { username: 'eca', password: '7' },
+  { username: 'arroyo', password: '8' },
+	{ username: 'jesus', password: '9' },
+];
+
+export default users;
