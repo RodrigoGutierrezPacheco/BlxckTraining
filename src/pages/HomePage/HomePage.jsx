2913c2c855d2eec91c8474cdc73472a6829689d4
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2023-07-03 19:22:30 -0600

    modificacion rutina

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -11,9 +11,9 @@ function HomePage() {
 			<div className="width">
 			<div className="box1">
        <img className="blxck" src="images/blxckLogo.png" alt="" />
-			 <h1 className="red justify q">!Unete a nuestra comunidad!</h1>
+			 <h1 className="red justify q">!Únete a nuestra comunidad!</h1>
 			 <h3 className="white justify w">Nuestro programa de entrenamiento es un conjunto de métodos, actividades y ejercicios que sirven para desarrollar la condición física. un buen entrenamiento junto con una buena recuperación, son la clave para cumplir tus objetivos.</h3>
-			 <motion.button whileTap={{scale:1.1}} whileHover={{scale:1.1}} className="button1 ins" onClick={whatsapp}>!Inscribete ahora!</motion.button>
+			 <motion.button whileTap={{scale:1.1}} whileHover={{scale:1.1}} className="button1 ins" onClick={whatsapp}>!Inscríbete ahora!</motion.button>
 			</div>
 			<div>
 				<h1 className="red e">¿Que incluyen nuestros programas de entrenamiento?</h1>
@@ -30,16 +30,16 @@ function HomePage() {
 				<div className="box2 row">
 					<h1 className="white t">3.-</h1>
 					<img className="contacto" src="images/contacto.png" alt="" />
-					<h1 className="white r">Seguimiento diario con los coaches.</h1>
+					<h1 className="white r">Seguimiento diario con los entrenadores.</h1>
 				</div>
 				<div className="box3 row">
 					<h1 className="black t">4.-</h1>
 					<img className="medida" src="images/medida.png" alt="" />
-					<h1 className="black r">Medidas Antropometricas. Mediciones de pliegues cutáneos para la valoración de la cantidad del tejido adiposo subcutáneo <br /><a className="red">*costo extra*</a></h1>
+					<h1 className="black r">Medidas Antropométricas. Mediciones de pliegues cutáneos para la valoración de la cantidad del tejido adiposo subcutáneo <br /><a className="red">*costo extra*</a></h1>
 				</div>
 				<h1 className="red ">¿Estas buscando un cambio en tu vida?</h1>
-				<h2 className="justify y">¡Únete ya! a nuestro programa de entrenamiento de hasta 5 semanas, todas nuestras rutinas estan diseñadas especificamente para ti. Contactanos para mayor informacion</h2>
-				<motion.button whileTap={{scale:1.1}} whileHover={{scale:1.1}} className="button1" onClick={whatsapp}>¡Contactanos!</motion.button>
+				<h2 className="justify y">¡Únete ya! a nuestro programa de entrenamiento de hasta 5 semanas, todas nuestras rutinas están diseñadas específicamente para ti. Contáctanos para mayor información</h2>
+				<motion.button whileTap={{scale:1.1}} whileHover={{scale:1.1}} className="button1" onClick={whatsapp}>¡Contáctanos!</motion.button>
 			</div>
 			<footer className="footer">
 				<h1 className="lFooter">Blxck Training</h1>
