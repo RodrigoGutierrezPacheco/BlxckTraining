1595a72e37de49587ff729177b0a2cba21198f95
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2023-12-04 21:02:02 -0600

    modificacion rutina

--- src/pages/Rutinas/Alejandra.jsx
+++ src/pages/Rutinas/Alejandra.jsx
@@ -1101,7 +1101,7 @@ const Alejandra = () => {
       {/* <h1 className='title red'>Estás en la ultima semana de tu entrenamiento</h1> */}
       {/* -----------------RUTINA NIVEL 1 SIN LESIONES PRINCIPIANTE ------ */}
       {/* Rutina Semana #1  */}
-      <h1>Intensidad entre 60%-70%</h1>
+      <h1>Intensidad Moderada</h1>
       <Collapsible
         open={false}
         trigger="Semana 1"
@@ -1299,7 +1299,7 @@ const Alejandra = () => {
       </Collapsible>
       {/* Rutina Semana #2  */}
        <Collapsible
-        open={true}
+        open={false}
         className="dia black1"
         trigger="Semana 2"
         triggerWhenOpen="Semana 2"
@@ -1494,8 +1494,8 @@ const Alejandra = () => {
         </div>
       </Collapsible> 
       {/*Rutina Semana #3*/}
-      {/* <Collapsible
-        open={false}
+       <Collapsible
+        open={true}
         className="dia"
         trigger="Semana 3"
         triggerWhenOpen="Semana 3"
@@ -1678,7 +1678,7 @@ const Alejandra = () => {
           </p>
           <p className="red">*Termina con programa cardio S8-Principiante-Nivel 3</p>
         </div>
-      </Collapsible> */}
+      </Collapsible> 
       {/* Rutina semana 4 */}
       {/* <Collapsible
         open={false}
