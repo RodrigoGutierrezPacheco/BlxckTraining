1e9df672f52222745aead8524f9ef2c824d64b0b
RodrigoGutierrezPacheco <rodrigogutierrezpacheco@gmail.com>
2024-09-29 18:47:29 -0600

    actyualizacion

--- src/pages/Rutinas/Alejandra.jsx
+++ src/pages/Rutinas/Alejandra.jsx
@@ -2001,7 +2001,7 @@ const Alejandra = () => {
         </div>
       </Collapsible> */}
       {/* Rutina semana 4 */}
-       <Collapsible
+       {/* <Collapsible
         open={true}
         className="dia black1"
         trigger="Semana 4"
@@ -2240,9 +2240,9 @@ const Alejandra = () => {
             *Termina con programa cardio S8-Principiante-Nivel2
           </p>
         </div>
-      </Collapsible> 
+      </Collapsible>  */}
       {/* Rutina semana 5 */}
-      {/* <Collapsible
+       <Collapsible
         open={true}
         className="dia"
         trigger="Semana 5"
@@ -2427,7 +2427,7 @@ const Alejandra = () => {
           <p className="red">*Termina con programa bicicleta 10min ritmo ligero*</p>
         </div>
 
-      </Collapsible>   */}
+      </Collapsible>  
       <button onClick={whatsapp} className="button2">
         ¿Tienes alguna duda o problema con tu rutina?
       </button>
